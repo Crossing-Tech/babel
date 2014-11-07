69b4a363ab397976e5172af20d800870c12d8d07
Christophe Pache <christophe.pache@crossing-tech.com>
2014-11-07 08:43:07 +0100

    proposal for keyword internal structure

--- babel-camel/babel-camel-mock/src/main/scala/io/xtech/babel/camel/mock/MockDSL.scala
+++ babel-camel/babel-camel-mock/src/main/scala/io/xtech/babel/camel/mock/MockDSL.scala
@@ -11,39 +11,37 @@
 package io.xtech.babel.camel.mock
 
 import io.xtech.babel.camel.parsing.CamelParsing
-import io.xtech.babel.fish.{ DSL2BaseDSL, BaseDSL }
 import io.xtech.babel.fish.model.StepDefinition
 import io.xtech.babel.fish.parsing.StepInformation
+import io.xtech.babel.fish.{ BaseDSL, DSL2BaseDSL }
 
 import org.apache.camel.model.ProcessorDefinition
 
 import scala.language.implicitConversions
 import scala.reflect.ClassTag
 
-case class MockDefinition(filePath: String) extends StepDefinition
-
-class MockDSL[I: ClassTag](protected val baseDsl: BaseDSL[I]) extends DSL2BaseDSL[I] {
-
-  /**
-    * The mock keyword. Stores received exchanges in order to assert properties in tests.
-    * @param endpointUri target mock endpoint which translates to "mock:endpointUri"
-    * @see  io.xtech.babel.camel.mock
-    * @return the possibility to add other steps to the current DSL
-    */
-  def mock(endpointUri: String): BaseDSL[I] = MockDefinition(endpointUri)
-}
-
 trait Mock extends CamelParsing {
 
-  abstract override def steps = super.steps :+ parse
+  case class MockDefinition(filePath: String) extends StepDefinition
 
-  val parse: Process = {
+  abstract override def steps = super.steps :+ {
     case StepInformation(MockDefinition(uri), camelProcessor: ProcessorDefinition[_]) =>
       camelProcessor.to(s"mock:$uri")
-
   }
 
-  implicit def mockDSLExtension[I: ClassTag](baseDsl: BaseDSL[I]) = new MockDSL(baseDsl)
+  implicit def mockDSLExtension[I: ClassTag](dsl: BaseDSL[I]) = new DSL2BaseDSL[I] {
+
+    protected val baseDsl = dsl
+
+    /**
+      * The mock keyword. Stores received exchanges in order to assert properties in tests.
+      * @param endpointUri target mock endpoint which translates to "mock:endpointUri"
+      * @see  io.xtech.babel.camel.mock
+      * @return the possibility to add other steps to the current DSL
+      */
+    def mock(endpointUri: String): BaseDSL[I] = MockDefinition(endpointUri)
+  }
+
 }
 
 //#doc:babel-mock
