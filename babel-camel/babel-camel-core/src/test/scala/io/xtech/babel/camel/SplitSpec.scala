e4549ee7f3cf68fbb8dcc6bbc2c400b47cd5b332
Christophe Pache <christophe.pache@crossing-tech.com>
2015-08-10 13:23:30 +0200

    fix ids for choice and when

--- babel-camel/babel-camel-core/src/test/scala/io/xtech/babel/camel/SplitSpec.scala
+++ babel-camel/babel-camel-core/src/test/scala/io/xtech/babel/camel/SplitSpec.scala
@@ -8,6 +8,7 @@
 
 package io.xtech.babel.camel
 
+import io.xtech.babel.camel.model.ReduceBodyAggregationStrategy
 import io.xtech.babel.camel.test.camel
 import org.apache.camel.builder.{ RouteBuilder => CRouteBuilder }
 import org.apache.camel.component.mock.MockEndpoint
@@ -109,6 +110,136 @@ class SplitSpec extends SpecificationWithJUnit {
 
   }
 
+  "a splitFold that throws an exception and stops" in new camel {
+
+    import io.xtech.babel.camel.builder.RouteBuilder
+
+    //#doc:babel-camel-split-reduce
+    val routeDef = new RouteBuilder {
+      //message bodies are converted to String if required
+      from("direct:babel").as[String].splitFoldBody(_.split(",").iterator, stopOnException = true){
+        _.to("mock:babelInner").requireAs[String].
+          processBody(x => if (x == "2") { throw new Exception("exceptected exception") } else { x }).
+          processBody(x => x.toInt).
+          to("mock:after").requireAs[Int]
+      }(1)((a,b) => {
+        a / b
+      }).
+        to("mock:babel")
+    }
+    //#doc:babel-camel-split-reduce
+
+    val aggregation = new ReduceBodyAggregationStrategy[String]((a,b) => s"$a, $b")
+
+    val camelRoute = new CRouteBuilder {
+      def configure(): Unit = {
+        from("direct:camel").split(body().tokenize(","), aggregation).stopOnException().
+          to("mock:camelInner").
+          process(new Processor {
+            override def process(exchange: Exchange): Unit = {
+              if (exchange.getIn.getBody == "2") {
+                throw new Exception("expected camel exception")
+              }
+            }
+          }).to("mock:camel1").end().to("mock:camel2")
+      }
+    }
+
+    routeDef.addRoutesToCamelContext(camelContext)
+    camelContext.addRoutes(camelRoute)
+    camelContext.start()
+
+    val producer = camelContext.createProducerTemplate()
+
+    val mockBabel1 = camelContext.getEndpoint("mock:babel").asInstanceOf[MockEndpoint]
+    val mockBabelafter = camelContext.getEndpoint("mock:after").asInstanceOf[MockEndpoint]
+    val mockBabelInner = camelContext.getEndpoint("mock:babelInner").asInstanceOf[MockEndpoint]
+    mockBabel1.expectedBodiesReceived()
+    mockBabelInner.expectedBodiesReceived("1", "2")
+
+    val mockCamel1 = camelContext.getEndpoint("mock:camel1").asInstanceOf[MockEndpoint]
+    val mockCamel2 = camelContext.getEndpoint("mock:camel2").asInstanceOf[MockEndpoint]
+    val mockCamelInner = camelContext.getEndpoint("mock:camelInner").asInstanceOf[MockEndpoint]
+    mockCamel1.expectedBodiesReceived("1")
+    mockCamel2.expectedBodiesReceived()
+    mockCamelInner.expectedBodiesReceived("1", "2")
+
+    producer.sendBody("direct:babel", "1,2,3") should throwA[Exception]
+    producer.sendBody("direct:camel", "1,2,3") should throwA[Exception]
+
+    mockCamel1.assertIsSatisfied()
+    mockCamelInner.assertIsSatisfied()
+    mockCamel2.assertIsSatisfied()
+    mockBabel1.assertIsSatisfied()
+    mockBabelInner.assertIsSatisfied()
+
+  }
+
+  "a splitReduce that throws an exception and stops" in new camel {
+
+    import io.xtech.babel.camel.builder.RouteBuilder
+
+    //#doc:babel-camel-split-reduce
+    val routeDef = new RouteBuilder {
+      //message bodies are converted to String if required
+      from("direct:babel").as[String].splitReduceBody(_.split(",").iterator, stopOnException = true){
+        _.to("mock:babelInner").requireAs[String].
+          processBody(x => if (x == "2") { throw new Exception("exceptected exception") } else { x }).
+          processBody(x => x.toInt).
+          to("mock:after").requireAs[Int]
+      }((a,b) => {
+        a / b
+      }).
+        to("mock:babel")
+    }
+    //#doc:babel-camel-split-reduce
+
+    val aggregation = new ReduceBodyAggregationStrategy[String]((a,b) => s"$a, $b")
+
+    val camelRoute = new CRouteBuilder {
+      def configure(): Unit = {
+        from("direct:camel").split(body().tokenize(","), aggregation).stopOnException().
+          to("mock:camelInner").
+          process(new Processor {
+          override def process(exchange: Exchange): Unit = {
+            if (exchange.getIn.getBody == "2") {
+              throw new Exception("expected camel exception")
+            }
+          }
+        }).to("mock:camel1").end().to("mock:camel2")
+      }
+    }
+
+    routeDef.addRoutesToCamelContext(camelContext)
+    camelContext.addRoutes(camelRoute)
+    camelContext.start()
+
+    val producer = camelContext.createProducerTemplate()
+
+    val mockBabel1 = camelContext.getEndpoint("mock:babel").asInstanceOf[MockEndpoint]
+    val mockBabelafter = camelContext.getEndpoint("mock:after").asInstanceOf[MockEndpoint]
+    val mockBabelInner = camelContext.getEndpoint("mock:babelInner").asInstanceOf[MockEndpoint]
+    mockBabel1.expectedBodiesReceived()
+    mockBabelInner.expectedBodiesReceived("1", "2")
+
+    val mockCamel1 = camelContext.getEndpoint("mock:camel1").asInstanceOf[MockEndpoint]
+    val mockCamel2 = camelContext.getEndpoint("mock:camel2").asInstanceOf[MockEndpoint]
+    val mockCamelInner = camelContext.getEndpoint("mock:camelInner").asInstanceOf[MockEndpoint]
+    mockCamel1.expectedBodiesReceived("1")
+    mockCamel2.expectedBodiesReceived()
+    mockCamelInner.expectedBodiesReceived("1", "2")
+
+    producer.sendBody("direct:babel", "1,2,3") should throwA[Exception]
+    producer.sendBody("direct:camel", "1,2,3") should throwA[Exception]
+
+    mockCamel1.assertIsSatisfied()
+    mockCamelInner.assertIsSatisfied()
+    mockCamel2.assertIsSatisfied()
+    mockBabel1.assertIsSatisfied()
+    mockBabelInner.assertIsSatisfied()
+
+  }
+
   "a split that throws an exception and stops" in new camel {
 
     import io.xtech.babel.camel.builder.RouteBuilder
@@ -126,12 +257,12 @@ class SplitSpec extends SpecificationWithJUnit {
       def configure(): Unit = {
         from("direct:camel").split(body().tokenize(",")).stopOnException().
           process(new Processor {
-            override def process(exchange: Exchange): Unit = {
-              if (exchange.getIn.getBody == "2") {
-                throw new Exception("expected camel exception")
-              }
+          override def process(exchange: Exchange): Unit = {
+            if (exchange.getIn.getBody == "2") {
+              throw new Exception("expected camel exception")
             }
-          }).to("mock:camel1").end().to("mock:camel2")
+          }
+        }).to("mock:camel1").end().to("mock:camel2")
       }
     }
 
